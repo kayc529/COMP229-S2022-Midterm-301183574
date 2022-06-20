cdfd239a26fc0a581084975a6242eee42d3ad7dc
Kay <kaicipher966@gmail.com>
2022-06-20 14:24:34 -0400

    completed add function

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -9,62 +9,67 @@ let movies = require('../models/movies');
 /* GET movies List page. READ */
 router.get('/', (req, res, next) => {
   // find all movie in the books collection
-  movies.find( (err, list) => {
+  movies.find((err, list) => {
     if (err) {
       return console.error(err);
-    }
-    else {
+    } else {
       res.render('movies/index', {
         title: 'Movies',
-        list: list
+        list: list,
       });
     }
   });
-
 });
 
 //  GET the Movies Details page in order to add a new Movies
 router.get('/add', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+  res.render('movies/details', {
+    title: 'Add a Movie',
+    list: {},
+  });
 });
 
 // POST process the Movies Details page and create a new Movies - CREATE
 router.post('/add', (req, res, next) => {
+  let newMovie = {
+    Title: req.body.title,
+    Description: req.body.description,
+    Released: req.body.released,
+    Director: req.body.director,
+    Genre: req.body.genre,
+  };
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  console.log('NEW MOVIE', newMovie);
 
+  movies.create(newMovie, (err, movie) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    }
+
+    res.redirect('/movies');
+  });
 });
 
 // GET the Movies Details page in order to edit an existing Movies
 router.get('/:id', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  /*****************
+   * ADD CODE HERE *
+   *****************/
 });
 
 // POST - process the information passed from the details form and update the document
 router.post('/:id', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+  /*****************
+   * ADD CODE HERE *
+   *****************/
 });
 
 // GET - process the delete by user id
 router.get('/delete/:id', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  /*****************
+   * ADD CODE HERE *
+   *****************/
 });
-
 
 module.exports = router;
