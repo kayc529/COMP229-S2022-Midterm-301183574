9b3ea285f106b0072e7080407381193d8acb2044
Kay <kaicipher966@gmail.com>
2022-06-21 14:21:11 -0400

    added comment

--- server/models/movies.js
+++ server/models/movies.js
@@ -1,15 +1,16 @@
 let mongoose = require('mongoose');
 
-// create a model class
-let Movies = mongoose.Schema({
+let Movies = mongoose.Schema(
+  {
     Title: String,
     Description: String,
     Released: Number,
     Director: String,
-    Genre: String
-},
-{
-  collection: "movies"
-});
+    Genre: String,
+  },
+  {
+    collection: 'movies',
+  }
+);
 
 module.exports = mongoose.model('Movies', Movies);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,13 @@
 // modules required for routing
 let express = require('express');
 let router = express.Router();
-let mongoose = require('mongoose');
-
-// define the game model
-let movies = require('../models/movies');
 
 /* GET home page. wildcard */
 router.get('/', (req, res, next) => {
   res.render('content/index', {
     title: 'Home',
-    movies: ''
-   });
+    movies: '',
+  });
 });
 
 module.exports = router;
